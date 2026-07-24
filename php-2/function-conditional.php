6bb0792966d53315fcaa09bcb412be301e28e983
dhifanisrinaa <dhifanisrinaa@gmail.com>
2026-07-24 15:46:37 +0700

    function-conditional.php

--- php-2/function-conditional.php
+++ php-2/function-conditional.php
@@ -42,19 +42,6 @@ reverseString("We Are JCC Developers");
 echo "<br>";
 
 echo "<h3>Soal No 3 Palindrome </h3>";
-/* 
-Soal No 3 
-Palindrome
-Buatlah sebuah function yang menerima parameter berupa string yang mengecek apakah string tersebut sebuah palindrome atau bukan. 
-Palindrome adalah sebuah kata atau kalimat yang jika dibalik akan memberikan kata yang sama contohnya: katak, civic.
-Jika string tersebut palindrome maka akan mengembalikan nilai true, sedangkan jika bukan palindrome akan mengembalikan false.
-NB: 
-Contoh: 
-palindrome("katak") => output : "true"
-palindrome("jambu") => output : "false"
-NB: DILARANG menggunakan built-in function PHP seperti strrev() dll. Gunakan looping seperti biasa atau gunakan function reverseString dari jawaban no.2!
-
-*/
 
 function palindrome($kata1) {
     $hasil1 = "";
