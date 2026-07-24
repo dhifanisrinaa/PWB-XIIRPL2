db77afbba2e4f10403db635f17695a76cc094f3b
dhifanisrinaa <dhifanisrinaa@gmail.com>
2026-07-24 15:45:42 +0700

    function-conditional.php

--- php-2/function-conditional.php
+++ php-2/function-conditional.php
@@ -13,45 +13,32 @@
 <?php
 
 echo "<h3> Soal No 1 Greetings </h3>";
-/* 
-Soal No 1
-Greetings
-Buatlah sebuah function greetings() yang menerima satu parameter berupa string. 
 
-contoh: greetings("abduh");
-Output: "Halo Abduh, Selamat Datang di Jabar Coding Camp!"
-*/
+function greetings($nama) {
+    $namaFormatted = ucwords(strtolower($nama));
+    
+    echo "Halo $namaFormatted, Selamat Datang di Jabar Coding Camp!<br>";
+}
 
-// Code function di sini
-
-
-// Hapus komentar untuk menjalankan code!
-// greetings("Bagas");
-// greetings("Wahyu");
-// greetings("Abdul");
-
+greetings("Bagas");
+greetings("Wahyu");
+greetings("Abdul");
 echo "<br>";
 
 echo "<h3>Soal No 2 Reverse String</h3>";
-/* 
-Soal No 2
-Reverse String
-Buatlah sebuah function reverseString() untuk mengubah string berikut menjadi kebalikannya menggunakan function dan looping (for/while/do while).
-Function reverseString menerima satu parameter berupa string.
-NB: DILARANG menggunakan built-in function PHP sepert strrev(), HANYA gunakan LOOPING!
 
-reverseString("abdul");
-Output: ludba
+function reverseString($kata) {
+    $panjangKata = strlen($kata);
+    $hasil = "";
+    for ($i = $panjangKata - 1; $i >= 0; $i--) {
+        $hasil .= $kata[$i];
+    }
+    echo $hasil . "<br>";
+}
 
-*/
-
-// Code function di sini 
-
-
-// Hapus komentar di bawah ini untuk jalankan Code
-// reverseString("abduh");
-// reverseString("Bootcamp");
-// reverseString("We Are JCC Developers")
+reverseString("abduh");
+reverseString("Bootcamp");
+reverseString("We Are JCC Developers");
 echo "<br>";
 
 echo "<h3>Soal No 3 Palindrome </h3>";
@@ -69,32 +56,47 @@ NB: DILARANG menggunakan built-in function PHP seperti strrev() dll. Gunakan loo
 
 */
 
+function palindrome($kata1) {
+    $hasil1 = "";
+    $panjangKata1 = strlen($kata1);
 
-// Code function di sini
+    for ($i1 = $panjangKata1 - 1; $i1 >= 0; $i1--) {
+        $hasil1 .= $kata1[$i1];
+    }
 
-// Hapus komentar di bawah ini untuk jalankan code
-// palindrome("civic") ; // true
-// palindrome("nababan") ; // true
-// palindrome("jambaban"); // false
-// palindrome("racecar"); // true
+    if ($kata1 === $hasil1) {
+        echo "true<br>";
+        return true;
+    } else {
+        echo "false<br>";
+        return false;
+    }
+}
+
+palindrome("civic") ; // true
+palindrome("nababan") ; // true
+palindrome("jambaban"); // false
+palindrome("racecar"); // true
 
 
 echo "<h3>Soal No 4 Tentukan Nilai </h3>";
-/*
-Soal 4
-buatlah sebuah function bernama tentukan_nilai . Di dalam function tentukan_nilai yang menerima parameter 
-berupa integer. dengan ketentuan jika paramater integer lebih besar dari sama dengan 85 dan lebih kecil sama dengan 100 maka akan mereturn String “Sangat Baik” 
-Selain itu jika parameter integer lebih besar sama dengan 70 dan lebih kecil dari 85 maka akan mereturn string “Baik” selain itu jika parameter number lebih besar 
-sama dengan 60 dan lebih kecil dari 70 maka akan mereturn string “Cukup” selain itu maka akan mereturn string “Kurang”
-*/
 
-// Code function di sini
+function tentukan_nilai($nilai) {
+    if ($nilai >= 85 && $nilai <= 100) {
+        return "Sangat Baik<br>";
+    } else if ($nilai >= 70 && $nilai < 85) {
+        return "Baik<br>";
+    } else if ($nilai >= 60 && $nilai < 70) {
+        return "Cukup<br>";
+    } else {
+        return "Kurang";
+    }
+}
 
-// Hapus komentar di bawah ini untuk jalankan code
-// echo tentukan_nilai(98); //Sangat Baik
-// echo tentukan_nilai(76); //Baik
-// echo tentukan_nilai(67); //Cukup
-// echo tentukan_nilai(43); //Kurang
+echo tentukan_nilai(98); //Sangat Baik
+echo tentukan_nilai(76); //Baik
+echo tentukan_nilai(67); //Cukup
+echo tentukan_nilai(43); //Kurang
 
 
 ?>
