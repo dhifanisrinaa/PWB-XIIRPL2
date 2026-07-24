368c3e1702c3592b0a8d7f4dad33e4f4e0a399ca
dhifanisrinaa <dhifanisrinaa@gmail.com>
2026-07-24 16:12:14 +0700

    index.php

--- oop/index.php
+++ oop/index.php
@@ -1,15 +1,29 @@
 <?php
-// Bentuk fungsi
-require ('animal.php');
 
-// Bentuk statement
-require 'animal.php';
+require_once "animal.php";
+require_once "Ape.php";
+require_once "Frog.php";
 
 $sheep = new Animal("shaun");
+echo "Name: " . $sheep->name . "<br>"; // "shaun"
+echo "Legs: " . $sheep->legs . "<br>"; // 4
+echo "Cold Blooded: " . $sheep->cold_blooded . "<br>"; // "no"
+echo "<br>";
 
-echo $sheep->name; // "shaun"
-echo $sheep->legs; // 4
-echo $sheep->cold_blooded; // "no"
+$sungokong = new Ape("kera sakti");
+echo "Name: " . $sungokong->name . "<br>";     // kera sakti
+echo "Legs: " . $sungokong->legs . "<br>";     // 2
+echo "Cold Blooded: " . $sungokong->cold_blooded . "<br>"; // no
+echo "Yell: ";
+$sungokong->yell();                            // Auooo
+echo "<br><br>";
 
-// NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
+$kodok = new Frog("buduk");
+echo "Name: " . $kodok->name . "<br>";         // buduk
+echo "Legs: " . $kodok->legs . "<br>";         // 4
+echo "Cold Blooded: " . $kodok->cold_blooded . "<br>"; // no
+echo "Jump: ";
+$kodok->jump();                                // Hop Hop
+echo "<br>";
+
 ?>
